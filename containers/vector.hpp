ad3a8dc6395c0444ff30852fd2a423103aa42a1c
KIM JEONGJUN <kimjeongjun@KIMs-MacBook-Pro.local>
2022-06-27 02:17:58 +0900

    added __vector_base_common

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -5,8 +5,29 @@
 
 __LIBFT_CONTAINERS_START__
 
-template <class T, class Alloc = allocator<T>>
-class vector {
+class __vector_base_common {
+    protected :
+        __vector_base_common() {};
+        __vector_base_common(const __vector_base_common&) {};
+        __vector_base_common& operator=(const __vector_base_common&) {};
+        ~__vector_base_common() {};
+        
+        void __throw_length_error() const {
+            throw std::length_error("exception from [ft::vector] : length error");
+        }
+
+        void __throw_out_of_range() const {
+            throw std::out_of_range("exception from [ft::vector] : out of range");
+        }
+};
+
+template <class T, class _Alloc>
+class _vector_base : protected __vector_base_common {
+    
+};
+
+template <class T, class _Allocator>
+class vector : private _vector_base <T, _Allocator> {
     public:
         typedef _Allocator                          allocator_type;
         typedef allocator_traits<allocator_type>    __alloc_traits;
@@ -18,7 +39,6 @@ class vector {
         typedef allocator_type::const_reference     const_reference;
         typedef allocator_type::pointer             pointer;
         typedef allocator_type::const_pointer       const_pointer;
-
         typedef ptrdiff_t                           difference_type;
 
     private :
