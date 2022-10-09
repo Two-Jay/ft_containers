aa3d0a2376e0d1985d5d221548f3fe566f40b93f
djeeee1272@gmail.com <djeeee1272@gmail.com>
2022-10-10 03:47:36 +0900

    add _length_check in vector_base

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -9,9 +9,9 @@ namespace ft {
     template <class _Tp, class _Alloc>
     class _vector_alloc_base {
         public :
-            typedef _Alloc                          allocator_type;
-            typedef _Tp*                            pointer;
-            typedef std::size_t                     size_type;
+            typedef _Alloc                                  allocator_type;
+            typedef _Tp*                                    pointer;
+            typedef std::size_t                             size_type;
 
             _vector_alloc_base (const allocator_type& __a)
                 : _alloc(__a), _start(0), _end_elements(0), _end_storage(0)
@@ -23,10 +23,10 @@ namespace ft {
             }
         
         protected :
-            allocator_type          _alloc;
-            pointer                 _start;
-            pointer                 _end_elements;
-            pointer                 _end_storage;
+            allocator_type                                  _alloc;
+            pointer                                         _start;
+            pointer                                         _end_elements;
+            pointer                                         _end_storage;
 
             pointer
             _allocate(size_type __n) {
@@ -74,26 +74,31 @@ namespace ft {
             
             void
             _range_check(size_type __n) const {
-                if (__n < this->size()) this->_throw_out_of_range();
+                if (__n >= this->size()) this->_throw_out_of_range();
+            }
+
+            void
+            _length_check(size_type __n) const {
+                if (__n > this->_get_capcity()) this->_throw_length_error();
             }
     };
 
     template <class _Tp, class _Alloc>
     class vector : protected _vector_base<_Tp, _Alloc> {
         private :
-            typedef _vector_base<_Tp, _Alloc>                               _Base;
-            typedef vector<_Tp, _Alloc>                                     vector_type;
+            typedef _vector_base<_Tp, _Alloc>                                   _Base;
+            typedef vector<_Tp, _Alloc>                                         vector_type;
 
-        public :
-            typedef _Tp                                                     value_type;
-            typedef _Tp*                                                    pointer;
-            typedef const _Tp*                                              const_pointer;
-            typedef _Tp&                                                    reference;
-            typedef const _Tp&                                              const_reference;
-            typedef std::ptrdiff_t                                          difference_type;
+        public :    
+            typedef _Tp                                                         value_type;
+            typedef _Tp*                                                        pointer;
+            typedef const _Tp*                                                  const_pointer;
+            typedef _Tp&                                                        reference;
+            typedef const _Tp&                                                  const_reference;
+            typedef std::ptrdiff_t                                              difference_type;
 
-            typedef typename _Base::size_type                               size_type;
-            typedef typename _Base::allocator_type                          allocator_type;
+            typedef typename _Base::size_type                                   size_type;
+            typedef typename _Base::allocator_type                              allocator_type;
             typedef ft::random_access_iterator<pointer, vector_type>            iterator;
             typedef ft::random_access_iterator<const_pointer, vector_type>      const_iterator;
             typedef ft::reverse_iterator<iterator>                              reverse_iterator;
@@ -107,6 +112,12 @@ namespace ft {
             using _Base::_end_storage;
 
         public :
+
+            explicit vector (const allocator_type& __a = allocator_type()) : _Base(__a) {}
+
+            vector(size_type __n, const_reference __value, const allocator_type& __a = allocator_type()) : _Base(__n, __a) {
+                _end_elements = std::uninitialized_fill_n(_start, __n, __value);
+            }
 
             allocator_type
             get_allocator() const {
@@ -158,6 +169,16 @@ namespace ft {
             const_reference
             back () const {
                 return *(this->end() - 1);
+            }
+
+            pointer
+            data () {
+                return this->_start;
+            }
+
+            const_pointer
+            data () const {
+                return this->_start;
             }
 
             /*
